119cee23cad61285c0edbe9409e3c8a45be1a641
Tolobek <tsharsheev002@gmail.com>
2024-03-27 11:47:04 +0600

    add main

---
+++ script22.js
@@ -0,0 +1,31 @@
+const API = "https://65ab6a1efcd1c9dcffc659a4.mockapi.io/api/v1/services"
+
+let result = document.querySelector(".result")
+
+async function getService() {
+    try{
+        const res = await fetch(API)
+
+        const data = await res.json()
+        showService(data)
+
+        console.log(data);
+
+    }catch(error) {
+        console.log(error);
+    }
+}
+getService()
+
+function showService(data) {
+    result.innerHTML = data.map((item) => {
+        return `
+          <div class="service">
+              <img src="${item.image}" alt="image" />
+              <h1>${item.name}</h1>
+              <h1>${item.category}</h1>
+          </div>
+          `;
+      })
+      .join("");
+}
